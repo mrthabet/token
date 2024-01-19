bda5c5f6cf99b43a277f0c8918d1e457e15f0b82
HP <HP@DESKTOP-RGULHIU>
2024-01-19 03:27:34 +0200

    second

--- src/main/java/token/token.java
+++ src/main/java/token/token.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 
 public class token {   
 	private WebDriver driver;
+	//اختبار رفع الكود بالتعديلات الجديدة  علي جيت هاب
 
 @BeforeEach
 public static void main(String[] args) throws IOException {
